8159688b998daba46383779cb146b23eecb0fcd8
tarifi-hicham <tarifi.hicham@gmail.com>
2023-10-19 23:01:43 +0100

    create mapreduce class to handle data

---
+++ mapreduce/mr.py
@@ -0,0 +1,48 @@
+import json
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from statistics import median
+
+
+class WordCounter(MRJob):
+    def mapper(self, _, line):
+        post = json.loads(line)
+
+        account = post["Account"]["username"]
+        account_created_at = post["Account"]["created_at"]
+        account_url = post["Account"]["url"]
+        account_followers_count = post["Account"]["followers_count"]
+        account_following_count = post["Account"]["following_count"]
+        account_status_count = post["Account"]["statuses_count"]
+        post_favourites_count = post["favourites_count"]
+        post_visibility = post["visibility"]
+        post_media = post["media_attachments"]
+        
+        yield "user:"+account, 1
+        yield "created_at:"+str(account_created_at), 1
+        yield "url:"+account_url, 1
+        yield "followers_count:"+account, account_followers_count
+        yield "following_count:"+account, account_following_count
+        yield "status_count:"+account, account_status_count
+        yield "favourites_count:"+account, post_favourites_count
+        yield "visibility:"+post_visibility, 1
+        yield "media", 1
+
+    def reducer(self, key, values):
+        if "count" in str(key):
+            yield key, max(values)
+        else:
+            yield key, sum(values)
+
+    def steps(self):
+        return [
+            MRStep(
+                mapper=self.mapper,
+                reducer=self.reducer
+            )
+        ]
+
+
+if __name__ == '__main__':
+    WordCounter.run()
+        
