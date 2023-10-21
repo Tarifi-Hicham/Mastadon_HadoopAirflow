e3e0cf5caa55847ef9da8dc9b1b2cdaa4de79b5b
tarifi-hicham <tarifi.hicham@gmail.com>
2023-10-21 12:35:37 +0100

    update script of collecting data

--- extract/script.py
+++ extract/script.py
@@ -9,7 +9,7 @@ import time
 
 
 # this function will pass through to the HDFS client write()
-def send_data_to_hdfs(df: pd.DataFrame, data_type: str):
+def send_data_to_hdfs(df: pd.DataFrame):
     
     # Convert DataFrame to JSON format
     data = df.to_json(orient='records', lines=True)
@@ -22,11 +22,7 @@ def send_data_to_hdfs(df: pd.DataFrame, data_type: str):
     # Connect to HDFS
     client = InsecureClient(host, user=user_name)
 
-    if data_type == 'users':
-        hdfs_filepath = "/user/" + user_name + "/" + datetime.now().strftime('%d%m%Y') + "/users" + datetime.now().strftime('%H%M%S') + ".json"
-    elif data_type == 'posts':
-        hdfs_filepath = "/user/" + user_name + "/" + datetime.now().strftime('%d%m%Y') + "/posts" + datetime.now().strftime('%H%M%S') + ".json"
-
+    hdfs_filepath = "/user/" + user_name + "/" + datetime.now().strftime('%d%m%Y') + "/posts" + datetime.now().strftime('%H%M%S') + ".json"
 
     # Upload the JSON data to HDFS
     with client.write(hdfs_filepath, overwrite=True) as hdfs_file:
@@ -41,33 +37,18 @@ access_token = os.getenv('access_token')
 
 # Create Mastodon API client
 mastodon = Mastodon(
-    api_base_url = 'https://mastodon.social',
-    access_token = access_token,
+    client_id=os.getenv('Client_key'),
+    client_secret=os.getenv('Client_secret'),
+    access_token=os.getenv('Access_token'),
+    api_base_url="https://mastodon.social"
 )
 
-def search_users():
-    data = []
-    for char in 'abcdefghijklmnopqrstuvwxyz':
-        results = mastodon.account_search(q=char)
-        for user in results:
-            user_data = {
-                'id': user['id'],
-                'username': user['username'],
-                'display_name': user['display_name'],
-                'is_group': user['group'],
-                'following_count': user['following_count'],
-                'followers_count': user['followers_count'],
-                'statuses_count': user['statuses_count'],
-                'bio': user['note'],
-                'is_bot': user['bot'],
-                'created_at': user['created_at']
-            }
-            data.append(user_data)
-    return data
 
-def get_posts():
+
+
+def get_posts(id):
     # toots = mastodon.timeline_hashtag(query,limit=10000)
-    toots = mastodon.timeline_public(limit=10000)
+    toots = mastodon.timeline_public(limit=10000,since_id=id)
     data = []
     for toot in toots:
         posts_data = {
@@ -76,6 +57,7 @@ def get_posts():
         'Content': toot['content'],
         'Created_at': toot['created_at'],
         'favourites_count': toot['favourites_count'],
+        'reblogs_count': toot['reblogs_count'],
         'sensitive': toot['sensitive'],
         'visibility': toot['visibility'],
         'mentions': toot['mentions'],
@@ -87,30 +69,34 @@ def get_posts():
         data.append(posts_data)
     return data
 
-
-# Extract users and save it to hdfs
-#print("Getting users from API")
-#users_data = search_users()  # search for users by a letter
-#df_users = pd.DataFrame(users_data)
-
-# Sending Files to hdfs
-#print("Sending file to hdfs")
-#send_data_to_hdfs(df_users, 'users')
+# iteration count 
+count = 20
 
 # Extract posts and save it to hdfs
 print("Getting posts from API")
 
-#posts_data = []
-# Extract posts and save it to hdfs
-#for i in range(5):
-#    posts = get_posts()  # Get the toots
-#    posts_data.append(posts)
-#    print("wait for 10sec ...")
-#    time.sleep(10)
+posts_data = []
 
-posts_data = get_posts()
+for i in range(count):
+    # Specify the last toot id
+    with open('extract/last_toot_id.txt', 'r+') as file:
+        last_toot_id = file.read().strip()
+
+    if last_toot_id != '':
+        last_toot_id = int(last_toot_id)
+    else:
+        last_toot_id = None
+
+    # Get Data from Mastodon API
+    data = get_posts(last_toot_id)
+    posts_data += data
+
+    # Save the last toot id
+    with open('extract/last_toot_id.txt', 'r+') as file:
+        file.write(str(posts_data[-1]['post_id']))
+    # print(last_toot_id)
 
 # Sending Files to hdfs
 print("Sending file to hdfs")
 df_posts = pd.DataFrame(posts_data)
-send_data_to_hdfs(df_posts, 'posts')
+send_data_to_hdfs(df_posts)
