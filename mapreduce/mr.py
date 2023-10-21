484bbbe4cdfbc14e716e490839980633e6eedd91
tarifi-hicham <tarifi.hicham@gmail.com>
2023-10-21 13:32:06 +0100

    add reblogs_count and language count

--- mapreduce/mr.py
+++ mapreduce/mr.py
@@ -15,8 +15,11 @@ class WordCounter(MRJob):
         account_following_count = post["Account"]["following_count"]
         account_status_count = post["Account"]["statuses_count"]
         post_favourites_count = post["favourites_count"]
+        post_reblogs_count = post["reblogs_count"]
         post_visibility = post["visibility"]
         post_media = post["media_attachments"]
+        language = post["language"]
+        post_tags = post["tags"]
         
         yield "user:"+account, 1
         yield "created_at:"+str(account_created_at), 1
@@ -25,8 +28,12 @@ class WordCounter(MRJob):
         yield "following_count:"+account, account_following_count
         yield "status_count:"+account, account_status_count
         yield "favourites_count:"+account, post_favourites_count
+        yield "reblogs_count:"+account, post_reblogs_count
         yield "visibility:"+post_visibility, 1
-        yield "media", 1
+        yield "language"+language, 1
+        yield "media", 1 if len(post_media) > 0 else 0
+        # for tag in post_tags:
+            # yield "tag:"+tag, 1
 
     def reducer(self, key, values):
         if "count" in str(key):
